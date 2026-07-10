e2397c0212dbf30a82be0e22656802bf185eec64
Shea <shea.wow@hotmail.com>
2026-07-10 10:09:40 -0400

    update code to remove all old logic and update README.md

--- README.md
+++ README.md
@@ -1,31 +1,10 @@
 # spfx-teamslink
 
 ## Summary
-
-- spfx-teamslink is a SPFX Application Customizer Extension that add a Conversations/Become a member link to community pages on gcxchange based on user membership to group. It also hide the teams icon next to the title.
-
-- You need to update the hubSiteIds property with comma seperated GUIDs for your valid hubs. You can do so in the serve.json file or from the tenant wide exstensions list when deployed.
-
-- You need to update the teamslinkId in TeamsLinkApplicationCustomizer.ts to avoid duplication.
-
-- You need to maitain a SharePoint list and store the value of teams id and teams link of each teams site where you want to apply this extension.
-
+- spfx-teamslink is a SPFX Application Customizer Extension that hides the join teams button for teams sites.
 
 ## Prerequisites
 - spfx-teamslink is intended to be deployed tenant wide.
-- A SharePoint list should created with the following fields and store the value of teams id and teams link of each teams site where you want to apply this extension. 
-
-Column Name|Field Type
--------|----
-`Teamslink`|Single line of text
-`TeamsID`|Single line of text
-
-- Created List's URL should given as value of TeamsListUrl in TeamsLinkApplicationCustomizer.ts
-
-## API permission
-- Microsoft Graph, User.ReadBasic.All
-- Microsoft Graph, Team.ReadBasic.All
-- Microsoft Graph, Channel.ReadBasic.All
 
 ## Version 
 ![SPFX](https://img.shields.io/badge/SPFX-1.17.4-green.svg)
@@ -43,6 +22,7 @@ Version|Date|Comments
 1.0|Dec 9, 2021|Initial release
 1.1|March 25, 2022|Next release
 1.3|October 25, 2023| Upgraded to SPFX 1.17.4
+1.4|July 10, 2026|Disable teamslink
 
 ## Minimal Path to Awesome
 - Clone this repository
@@ -50,14 +30,6 @@ Version|Date|Comments
 - Ensure the current version of the Node.js (16.3+)
   - **in the command-line run:**
     - **npm install**
-- Create a SharePoint list with the following fields and store the value of teams id and teams link of each teams site where you want to apply this extension. 
-
-Column Name|Field Type
--------|----
-`Teamslink`|Single line of text
-`TeamsID`|Single line of text
-
-- Give created list's url as the value of the TeamsListUrl in TeamsLinkApplicationCustomizer.ts. 
 
 - To debug
   - go to the `config\serve.json` file and update `pageUrl` to url of any teams site
@@ -71,11 +43,6 @@ Column Name|Field Type
     - **gulp package-solution --ship**
 
 - Upload the extension from `\sharepoint\solution` to your tenant's app store
-- To add or modify extension properties
-  - **Go to Modern Appcatalog**
-  - **Click ...More features in the left side**
-  - **Open the tenant wide Extension**
-  - **Edit the hubSiteIds under the title called TeamsLink**
 
 ## Disclaimer
 **THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
--- src/extensions/teamsLink/TeamsLinkApplicationCustomizer.ts
+++ src/extensions/teamsLink/TeamsLinkApplicationCustomizer.ts
@@ -6,41 +6,19 @@ import {
   BaseApplicationCustomizer
 } from '@microsoft/sp-application-base';
 
-import * as strings from 'TeamsLinkApplicationCustomizerStrings';
-
-import { graph } from "@pnp/graph";
-import "@pnp/graph/teams";
-import "@pnp/graph/users";
-import "@pnp/graph/groups";
-
-import styles from './components/TeamsLink.module.scss';
-
-import { SPHttpClient } from '@microsoft/sp-http';
-import { DisplayMode } from '@microsoft/sp-core-library';
-
-
 export interface ITeamsLinkApplicationCustomizerProperties {
-  TeamsListUrl: string;
-  hubSiteIds: string;
-  noTeamsLink: string;
+
 }
-
-
 
 export default class TeamsLinkApplicationCustomizer
   extends BaseApplicationCustomizer<ITeamsLinkApplicationCustomizerProperties> {
-
-  teamslinkId: string = "f3f79be5-ebc1-4ce5-8435-96db86a4eb20";
-
 
   @override
   public async onInit(): Promise<void> {
 
     await super.onInit();
 
-    //this.context.application.navigatedEvent.add(this, this.initialize);
     this.context.application.navigatedEvent.add(this, this.removeTeamsLink);
-
 
     window.addEventListener('click', (event) => {
       const el = event.target as HTMLElement;
@@ -48,17 +26,11 @@ export default class TeamsLinkApplicationCustomizer
       if (el.innerHTML === "Republish" || el.className.includes("ms-Icon ms-Button-icon") || el.className.includes("ms-Icon--ChromeClose")) {
         const interval = window.setInterval(() => {
         const teamsChannelButton = document.querySelector('button[title="Go to the Microsoft Teams channel"]');
-        //const customTeamsButton = document.querySelector(`button[title="${strings.conversations}"]`);
 
           if (teamsChannelButton !== null) {
             teamsChannelButton.remove();
             clearInterval(interval);
           }
-
-          // if (customTeamsButton === null) {
-          //   this.initialize();
-          // }
-
         }, 1000);
       }
     });
@@ -98,223 +70,10 @@ export default class TeamsLinkApplicationCustomizer
     return Promise.resolve();
   }
 
-
-
-
   public removeTeamsLink():void {
     const teamsChannelButton = document.querySelector('button[title="Go to the Microsoft Teams channel"]');
     if(teamsChannelButton) {
       teamsChannelButton.remove();
     }
   }
-
-  public async initialize():Promise<string|void> {
-    graph.setup({
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      spfxContext: this.context as any
-    });
-    // Check if a community site
-
-    if(!this.context.pageContext.legacyPageContext.isHubSite && this.checkHubSiteIds()){
-
-      const teamsUrl =  await this.getTeamURL();
-      const isMember = await this.isMember();
-
-      // Add conversations
-      this.render(teamsUrl, isMember);
-
-      const siteHeader = document.querySelector('[data-automationid="SiteHeader"]');
-      const layoutOption = siteHeader.attributes[2].value;
-
-      if (layoutOption === "1") {
-        siteHeader.lastElementChild.removeAttribute('style');
-        siteHeader.lastElementChild.classList.add(styles.inLineMenu);
-      }
-
-      // eslint-disable-next-line @typescript-eslint/no-this-alias
-      const context = this;
-      // Watch to see if elements change based on window size
-      const observer = new MutationObserver(function(mutations_list) {
-        console.log("mutationList", mutations_list);
-        mutations_list.forEach(function(mutation) {
-          console.log("mutation", mutation);
-          mutation.addedNodes.forEach(function(added_node) {
-            console.log("added_node",added_node)
-
-            // Desktop size
-            if(added_node.isSameNode(siteHeader.querySelector('[class^="actionsWrapper-"]'))){
-              if(!context.linkExists()) {
-                const actionLink = context.createLink(teamsUrl);
-                actionLink.className = styles.actionLinkBox;
-
-                if(isMember){
-                  actionLink.innerText = strings.conversations;
-                  actionLink.setAttribute("aria-label", strings.conversations);
-                  actionLink.setAttribute("title", strings.conversations);
-                } else {
-                  actionLink.innerText = strings.become;
-                  actionLink.setAttribute("aria-label", strings.become);
-                  actionLink.setAttribute("title", strings.become);
-                }
-
-                siteHeader.querySelector('[class^="actionsWrapper-"]').prepend(actionLink);
-              }
-            // Mobile size
-            } else if(added_node.isSameNode(siteHeader.querySelector('[class^="sideActionsWrapper-"]'))) {
-              if(!context.linkExists()) {
-                const actionLink = context.createLink(teamsUrl);
-
-                if(isMember){
-                  actionLink.innerText = strings.conversations;
-                  actionLink.setAttribute("aria-label", strings.conversations);
-                  actionLink.setAttribute("title", strings.conversations);
-                } else {
-                  actionLink.innerText = strings.become;
-                  actionLink.setAttribute("aria-label", strings.become);
-                  actionLink.setAttribute("title", strings.become);
-
-                }
-
-                context.applyMobileStyle();
-                siteHeader.querySelector('[class^="sideActionsWrapper-"]').prepend(actionLink);
-              }
-            }
-          });
-        });
-      });
-
-      const config = {
-        childList: true
-      };
-
-      observer.observe(siteHeader, config);
-    }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public render(teamsUrl:any, isMember:boolean):any {
-    if(this.linkExists())
-      return;
-
-    const actionLink = this.createLink(teamsUrl);
-    actionLink.className = styles.actionLinkBox;
-
-    if(isMember){
-      actionLink.innerText = strings.conversations;
-      actionLink.setAttribute("aria-label", strings.conversations);
-      actionLink.setAttribute("title", strings.conversations);
-    } else {
-      actionLink.innerText = strings.become;
-      actionLink.setAttribute("aria-label", strings.become);
-      actionLink.setAttribute("title", strings.become);
-    }
-
-    const actionsBar = document.querySelector('[class^="actionsWrapper-"]');
-    if(actionsBar){
-      //actionsBar.prepend(spacer);
-      actionsBar.prepend(actionLink);
-    } else {
-      this.applyMobileStyle();
-      document.querySelector('[class^="sideActionsWrapper-"]').prepend(actionLink)
-    }
-    //document.querySelector<HTMLElement>('[class^="ms-Button ms-Button--icon teamsChannelLink-"]').style.display = "none";
-    console.log("render")
-  }
-
-  public async getTeamURL():Promise<string|void> {
-    const TeamsListUrl = "https://devgcx.sharepoint.com/sites/app-reference/_api/lists/GetByTitle('TeamsLink')/items?$top=4000";
-    const noTeamsLink = "NOTEAMSLINK";
-    const groupid = this.context.pageContext.site.group.id._guid;
-    // Get teams link sharepoint list
-    const url = await this.context.spHttpClient.get(TeamsListUrl,
-    SPHttpClient.configurations.v1,
-    {
-      headers: {
-        "Accept": "application/json;odata=nometadata",
-        "odata-version": "3.0"
-      }
-    })
-    .then(function (response) {
-      if (!response.ok){
-          throw Error(`Error, status code ${response.status}`);
-      }else{
-          return response.json();
-      }
-    }).then(function(jsonObject){
-      const TeamsLinksList = jsonObject.value;
-      //Get all item and check if matching groupid
-      let teamslink = noTeamsLink
-      for (const item of TeamsLinksList) {
-        if(item.TeamsID === groupid){
-          teamslink = item.Teamslink
-          break;
-        }
-      }
-      return teamslink
-    }).catch(function (error) {
-      console.log(`Error:${error}`);
-    });
-    return Promise.resolve(url);
-  }
-
-  public async isMember():Promise<boolean>{
-    const groupid = this.context.pageContext.site.group.id._guid;
-    let isMember = false;
-
-    await graph.me.checkMemberGroups([groupid]).then(res => {
-      if(res[0] === groupid){
-        isMember = true;
-      }
-    });
-
-    return isMember;
-  }
-
-  private createLink(teamsUrl): HTMLAnchorElement {
-    const actionLink = document.createElement("a");
-
-    actionLink.href = teamsUrl;
-    actionLink.className = styles.actionsLink;
-    actionLink.target = "_blank";
-    actionLink.id = this.teamslinkId;
-
-    return actionLink;
-  }
-
-  private linkExists(): boolean {
-    const link = document.getElementById(this.teamslinkId);
-    return link !== null;
-  }
-
-  // Make sure the hub we're in is one of the approved hubs
-  private checkHubSiteIds(): boolean {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const context = this;
-    const hubSiteIds = `${this.properties.hubSiteIds}`.replace(/\s/g, '').split(',');
-    for(let i = 0; i < hubSiteIds.length; i++) {
-      if(context.context.pageContext.legacyPageContext.hubSiteId === hubSiteIds[i])
-        return true;
-    }
-
-    return false;
-  }
-
-  private applyMobileStyle(): void {
-    const actionWrapper = document.querySelector('[data-automationid="SiteHeader"]').querySelector('[class^="sideActionsWrapper-"]') as HTMLElement;
-    if(actionWrapper) {
-      actionWrapper.style.display = "inline";
-    }
-
-    const moreActions = actionWrapper.querySelector('[class^="moreActionsButton-"]') as HTMLElement;
-
-    if(moreActions){
-      moreActions.style.display = "inline";
-    }
-  }
-
-
-
 }
-
-
-
